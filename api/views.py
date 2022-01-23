ada4d688bad3234e8208edd0ed35a0ef0d25c2c2
Darania Muralitharan <Darania@Daranias-MacBook-Air.local>
2022-01-23 18:59:17 +0000

    custom model

--- api/views.py
+++ api/views.py
@@ -1,10 +1,18 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.response import Response
 from api.serializers import MovieSerializer, RatingSerializer
 from api.models import Movie, Rating
+from rest_framework.decorators import action
 
 class MovieViewSet(viewsets.ModelViewSet):
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
+
+    # making own custom model
+    @action(detail=True, methods=['POST'])
+    def rate_movie(self, request, pk=None):
+        response = {'message': 'it is working'}
+        return Response(response, status=status.HTTP_200_OK)
 
 class RatingViewSet(viewsets.ModelViewSet):
     queryset = Rating.objects.all()
