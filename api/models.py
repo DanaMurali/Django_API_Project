3a710c6e2aeba5691ae8cf78ff1535f073c3072c
Darania Muralitharan <Darania@Daranias-MacBook-Air.local>
2022-01-23 19:58:56 +0000

    Ratings - number of ratings and average ratings

--- api/models.py
+++ api/models.py
@@ -6,6 +6,22 @@ class Movie(models.Model):
     title = models.CharField(max_length=32)
     description = models.TextField(max_length=360)
 
+    def no_of_ratings(self):
+        # selecting all ratings in our db. and filtering for each movie
+        ratings = Rating.objects.filter(movie=self)
+        return len(ratings)
+
+    def avg_ratings(self):
+        sum = 0
+        ratings = Rating.objects.filter(movie=self)
+        for rating in ratings:
+            sum += rating.stars
+        if len(ratings) > 0:
+            return sum / len(ratings)
+        else:
+            return 0
+
+
 # if we delete the movie we will cascade and delete the rating as well.
 class Rating(models.Model):
     movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
