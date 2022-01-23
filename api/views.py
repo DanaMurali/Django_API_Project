34cae140acadce9f7d2100af0bb73eb581bf73fa
Darania Muralitharan <Darania@Daranias-MacBook-Air.local>
2022-01-23 18:35:12 +0000

    serializers

--- api/views.py
+++ api/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from api.serializers import MovieSerializer, RatingSerializer
+from api.models import Movie, Rating
 
-# Create your views here.
+class MovieViewSet(viewsets.ModelViewSet):
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
+
+class RatingViewSet(viewsets.ModelViewSet):
+    queryset = Rating.objects.all()
+    serializer_class = RatingSerializer
