c05fb21c81049d3a367739875f1352349dd4593b
Darania Muralitharan <Darania@Daranias-MacBook-Air.local>
2022-01-23 18:22:02 +0000

    creating models - movie, rating and user

--- api/models.py
+++ api/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import MaxValueValidator, MinValueValidator
 
-# Create your models here.
+class Movie(models.Model):
+    title = models.CharField(max_length=32)
+    description = models.TextField(max_length=360)
+
+# if we delete the movie we will cascade and delete the rating as well.
+class Rating(models.Model):
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    stars = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+    class Meta:
+        unique_together = (('user', 'movie'),)
+        index_together = (('user', 'movie'),)
+
